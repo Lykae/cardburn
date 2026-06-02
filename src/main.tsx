fcc028afe46a7398a51cb6025582c27db2b558c3
Mandl-Laptop <zwerg01@gmx.net>
2026-06-02 14:32:57 +0200

    status bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
-//import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+
+import { Capacitor } from "@capacitor/core";
+import { StatusBar, Style } from "@capacitor/status-bar";
+
+async function setup() {
+  await Capacitor.isNativePlatform();
+
+  await StatusBar.setOverlaysWebView({ overlay: true });
+  await StatusBar.setStyle({ style: Style.Dark });
+}
+
+setup();
 
 createRoot(document.getElementById("root")!).render(<App />);
