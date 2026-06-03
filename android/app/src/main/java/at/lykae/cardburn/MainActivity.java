d89ec57377a53caa84ed88149b775fe2b5198ab0
Mandl-Laptop <zwerg01@gmx.net>
2026-06-03 13:55:26 +0200

    add capacitor

---
+++ android/app/src/main/java/at/lykae/cardburn/MainActivity.java
@@ -0,0 +1,46 @@
+package at.lykae.cardburn;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        Window window = getWindow();
+
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        window.setStatusBarColor(Color.parseColor("#030712"));
+        window.setNavigationBarColor(Color.parseColor("#030712"));
+
+        View decorView = window.getDecorView();
+
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+}
